1ffd9ad7e795242217986c71feacc82c79fe004a
Edilson França <42967820+EdilsonFranca@users.noreply.github.com>
2021-11-11 00:26:56 -0300

    Add files via upload

---
+++ src/main/java/br/com/delivery/app/Config/WebConfig.java
@@ -0,0 +1,11 @@
+package br.com.delivery.app.Config;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
