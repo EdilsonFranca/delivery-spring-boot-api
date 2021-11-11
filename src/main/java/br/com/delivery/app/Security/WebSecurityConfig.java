2a0cdc64722f201988e43538d6ca31f3fa62fc6d
Edilson França <42967820+EdilsonFranca@users.noreply.github.com>
2021-11-10 23:10:02 -0300

    Update WebSecurityConfig.java

--- src/main/java/br/com/delivery/app/Security/WebSecurityConfig.java
+++ src/main/java/br/com/delivery/app/Security/WebSecurityConfig.java
@@ -52,6 +52,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		                 http.cors().and()
 				.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 				.authorizeRequests().antMatchers("/api/auth/**").permitAll()
