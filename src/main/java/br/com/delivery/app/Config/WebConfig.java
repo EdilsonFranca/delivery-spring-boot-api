c2c8040e12ab5f1ebc263666f1cba0704e1c4d85
Edilson França <42967820+EdilsonFranca@users.noreply.github.com>
2021-11-11 00:44:42 -0300

    Update WebConfig.java

--- src/main/java/br/com/delivery/app/Config/WebConfig.java
+++ src/main/java/br/com/delivery/app/Config/WebConfig.java
@@ -1,16 +1,5 @@
 package br.com.delivery.app.Config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+public class WebConfig {
 
-@Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
 }
