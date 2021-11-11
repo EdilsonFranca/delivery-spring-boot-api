beaa860270200758e73db368cdc1c6e8ae8d5296
Edilson França <42967820+EdilsonFranca@users.noreply.github.com>
2021-11-11 00:43:59 -0300

    Update DeliveryApplication.java

--- src/main/java/br/com/delivery/app/DeliveryApplication.java
+++ src/main/java/br/com/delivery/app/DeliveryApplication.java
@@ -2,7 +2,11 @@ package br.com.delivery.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class DeliveryApplication {
@@ -10,4 +14,15 @@ public class DeliveryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DeliveryApplication.class, args);
 	}
+	
+	   @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }
