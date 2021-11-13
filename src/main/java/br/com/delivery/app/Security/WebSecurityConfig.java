1b2741b51e2c115da825c3d61336b97f0e0a44bd
Edilson França <42967820+EdilsonFranca@users.noreply.github.com>
2021-11-12 22:14:04 -0300

    Update WebSecurityConfig.java

--- src/main/java/br/com/delivery/app/Security/WebSecurityConfig.java
+++ src/main/java/br/com/delivery/app/Security/WebSecurityConfig.java
@@ -53,7 +53,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		                 http.cors().and().csrf().disable()
+		                http.requiresChannel()
+                                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+                                .requiresSecure()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 				.authorizeRequests().antMatchers("/api/auth/**").permitAll()
 				.antMatchers("/api/dashboard").permitAll()
