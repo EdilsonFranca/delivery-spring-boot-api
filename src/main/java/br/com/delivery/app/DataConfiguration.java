844541dbcb0f9460e069508a1e9026205fb70420
Edilson França <42967820+EdilsonFranca@users.noreply.github.com>
2021-11-15 05:37:02 -0300

    Update DataConfiguration.java

--- src/main/java/br/com/delivery/app/DataConfiguration.java
+++ src/main/java/br/com/delivery/app/DataConfiguration.java
@@ -12,16 +12,16 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 @Configuration
 public class DataConfiguration {
 
-	@Bean
-    public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/u979687582_delivery_app?useTimezone=true&serverTimezone=UTC");
-        dataSource.setUsername("u979687582_delivery_admin");
-        dataSource.setPassword("[e5KL>9r");
-        return dataSource;
-    }
-	
+        @Bean
+	public DataSource dataSource(){
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://185.201.11.170:3306/u979687582_delivery_app?enabledTLSProtocols=TLSv1.2&useTimezone=true&serverTimezone=UTC");
+		dataSource.setUsername("u979687582_delivery_admin");
+		dataSource.setPassword("6$Pap]?9vY8L");
+		return dataSource;
+	}
+
 	@Bean
 	public JpaVendorAdapter jpaVendorAdapter(){
 		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
